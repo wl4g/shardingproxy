a23fa143db3d5d40893a995d112cc28ff4b6d03f
wl4g <983708408@qq.com>
2022-01-29 16:43:16 +0800

    fix:

--- xcloud-shardingproxy-starter/src/main/resources/example/interval-sharding-readwrite/interval-sharding-readwrite-paymentdb.sql
+++ xcloud-shardingproxy-starter/src/main/resources/example/interval-sharding-readwrite/interval-sharding-readwrite-paymentdb.sql
@@ -4,12 +4,10 @@ drop schema if exists paymentdb_r0z0mgr0db1;
 -- MGR (MySQL group replication) cluster 1
 drop schema if exists paymentdb_r0z0mgr1db0;
 drop schema if exists paymentdb_r0z0mgr1db1;
-drop schema if exists paymentdb_r0z0mgr1db2;
-drop schema if exists paymentdb_r0z0mgr1db3;
 -- Small database that do not participate in sharding.
 drop schema if exists paymentdb_r0z0mgr2_single;
 
--- --------------------------------- Sharding of MGR cluster 0 (matrix: 2db x 4table) --------------------------------------
+-- --------------------------------- Sharding of MGR cluster 0 --------------------------------------
 
 -- mgr0.db0
 drop schema if exists paymentdb_r0z0mgr0db0;
@@ -19,14 +17,14 @@ drop table if exists t_bill_0;
 drop table if exists t_bill_1;
 drop table if exists t_bill_2;
 drop table if exists t_bill_3;
-create table if not exists t_bill_0(id bigint not null, `name` varchar(64) character set utf8 collate utf8_bin, primary key(id))engine=innodb comment='所属虚拟库：paymentdb，所属分片组：group0，所属物理库：mgr0.db0，ID取值范围：[0~1000w)，库定位算法：id%8=0,1,2,3';
-create table if not exists t_bill_1(id bigint not null, `name` varchar(64) character set utf8 collate utf8_bin, primary key(id))engine=innodb comment='所属虚拟库：paymentdb，所属分片组：group0，所属物理库：mgr0.db0，ID取值范围：[1000w~2000w)，库定位算法：id%8=0,1,2,3';
-create table if not exists t_bill_2(id bigint not null, `name` varchar(64) character set utf8 collate utf8_bin, primary key(id))engine=innodb comment='所属虚拟库：paymentdb，所属分片组：group0，所属物理库：mgr0.db0，ID取值范围：[2000w~3000w)，库定位算法：id%8=0,1,2,3';
-create table if not exists t_bill_3(id bigint not null, `name` varchar(64) character set utf8 collate utf8_bin, primary key(id))engine=innodb comment='所属虚拟库：paymentdb，所属分片组：group0，所属物理库：mgr0.db0，ID取值范围：[3000w~4000w)，库定位算法：id%8=0,1,2,3';
-insert into `t_bill_0` (`id`, `name`) values (107,'skirt-107 from r0z0mgr0db0.t0');
-insert into `t_bill_1` (`id`, `name`) values (10000009,'skirt-10000009 from r0z0mgr0db0.t1');
-insert into `t_bill_2` (`id`, `name`) values (27000018,'skirt-27000018 from r0z0mgr0db0.t2');
-insert into `t_bill_3` (`id`, `name`) values (37000019,'skirt-37000019 from r0z0mgr0db0.t3');
+create table if not exists t_bill_0(id bigint not null, order_id bigint(20) NOT NULL, `lables` varchar(64) character set utf8 collate utf8_bin, primary key(id))engine=innodb comment='所属虚拟库：paymentdb，所属物理库：mgr0.db0';
+create table if not exists t_bill_1(id bigint not null, order_id bigint(20) NOT NULL, `lables` varchar(64) character set utf8 collate utf8_bin, primary key(id))engine=innodb comment='所属虚拟库：paymentdb，所属物理库：mgr0.db0';
+create table if not exists t_bill_2(id bigint not null, order_id bigint(20) NOT NULL, `lables` varchar(64) character set utf8 collate utf8_bin, primary key(id))engine=innodb comment='所属虚拟库：paymentdb，所属物理库：mgr0.db0';
+create table if not exists t_bill_3(id bigint not null, order_id bigint(20) NOT NULL, `lables` varchar(64) character set utf8 collate utf8_bin, primary key(id))engine=innodb comment='所属虚拟库：paymentdb，所属物理库：mgr0.db0';
+insert into `t_bill_0` (`id`, `order_id`, `lables`) values (107, 10000000, 'label-107 from r0z0mgr0db0.t0');
+insert into `t_bill_1` (`id`, `order_id`, `lables`) values (10000009, 10000001, 'label-10000009 from r0z0mgr0db0.t1');
+insert into `t_bill_2` (`id`, `order_id`, `lables`) values (27000018, 10000002, 'label-27000018 from r0z0mgr0db0.t2');
+insert into `t_bill_3` (`id`, `order_id`, `lables`) values (37000019, 10000003, 'label-37000019 from r0z0mgr0db0.t3');
 
 -- mgr0.db1
 drop schema if exists paymentdb_r0z0mgr0db1;
@@ -36,14 +34,14 @@ drop table if exists t_bill_0;
 drop table if exists t_bill_1;
 drop table if exists t_bill_2;
 drop table if exists t_bill_3;
-create table if not exists t_bill_0(id bigint not null, `name` varchar(64) character set utf8 collate utf8_bin, primary key(id))engine=innodb comment='所属虚拟库：paymentdb，所属分片组：group0，所属物理库：mgr0.db1，ID取值范围：[0~1000w)，库定位算法：id%8=4,5,6,7';
-create table if not exists t_bill_1(id bigint not null, `name` varchar(64) character set utf8 collate utf8_bin, primary key(id))engine=innodb comment='所属虚拟库：paymentdb，所属分片组：group0，所属物理库：mgr0.db1，ID取值范围：[1000w~2000w)，库定位算法：id%8=4,5,6,7';
-create table if not exists t_bill_2(id bigint not null, `name` varchar(64) character set utf8 collate utf8_bin, primary key(id))engine=innodb comment='所属虚拟库：paymentdb，所属分片组：group0，所属物理库：mgr0.db1，ID取值范围：[2000w~3000w)，库定位算法：id%8=4,5,6,7';
-create table if not exists t_bill_3(id bigint not null, `name` varchar(64) character set utf8 collate utf8_bin, primary key(id))engine=innodb comment='所属虚拟库：paymentdb，所属分片组：group0，所属物理库：mgr0.db1，ID取值范围：[3000w~4000w)，库定位算法：id%8=4,5,6,7';
-insert into `t_bill_0` (`id`, `name`) values (205,'skirt-205 from r0z0mgr0.t0');
-insert into `t_bill_1` (`id`, `name`) values (10000029,'skirt-10000029 from r0z0mgr0db1.t1');
-insert into `t_bill_2` (`id`, `name`) values (25000029,'skirt-25000029 from r0z0mgr0db1.t2');
-insert into `t_bill_3` (`id`, `name`) values (35000029,'skirt-35000029 from r0z0mgr0db1.t3');
+create table if not exists t_bill_0(id bigint not null, order_id bigint(20) NOT NULL, `lables` varchar(64) character set utf8 collate utf8_bin, primary key(id))engine=innodb comment='所属虚拟库：paymentdb，所属物理库：mgr0.db1，ID取值范围：[0~1000w)，库定位算法：id%8=4,5,6,7';
+create table if not exists t_bill_1(id bigint not null, order_id bigint(20) NOT NULL, `lables` varchar(64) character set utf8 collate utf8_bin, primary key(id))engine=innodb comment='所属虚拟库：paymentdb，所属物理库：mgr0.db1，ID取值范围：[1000w~2000w)，库定位算法：id%8=4,5,6,7';
+create table if not exists t_bill_2(id bigint not null, order_id bigint(20) NOT NULL, `lables` varchar(64) character set utf8 collate utf8_bin, primary key(id))engine=innodb comment='所属虚拟库：paymentdb，所属物理库：mgr0.db1，ID取值范围：[2000w~3000w)，库定位算法：id%8=4,5,6,7';
+create table if not exists t_bill_3(id bigint not null, order_id bigint(20) NOT NULL, `lables` varchar(64) character set utf8 collate utf8_bin, primary key(id))engine=innodb comment='所属虚拟库：paymentdb，所属物理库：mgr0.db1，ID取值范围：[3000w~4000w)，库定位算法：id%8=4,5,6,7';
+insert into `t_bill_0` (`id`, `order_id`, `lables`) values (205, 10000004, 'label-205 from r0z0mgr0.t0');
+insert into `t_bill_1` (`id`, `order_id`, `lables`) values (10000029, 10000005, 'label-10000029 from r0z0mgr0db1.t1');
+insert into `t_bill_2` (`id`, `order_id`, `lables`) values (25000029, 10000006, 'label-25000029 from r0z0mgr0db1.t2');
+insert into `t_bill_3` (`id`, `order_id`, `lables`) values (35000029, 10000007, 'label-35000029 from r0z0mgr0db1.t3');
 
 -- --------------------------------- Non Sharding single database --------------------------------------
 
