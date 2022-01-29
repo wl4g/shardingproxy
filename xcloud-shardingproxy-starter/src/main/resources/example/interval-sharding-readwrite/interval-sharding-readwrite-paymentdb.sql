d98a0cdb2b02b03179991101107ef470a879c9bb
wl4g <983708408@qq.com>
2022-01-29 23:15:19 +0800

    fix: autoInterval sharding config

--- xcloud-shardingproxy-starter/src/main/resources/example/interval-sharding-readwrite/interval-sharding-readwrite-paymentdb.sql
+++ xcloud-shardingproxy-starter/src/main/resources/example/interval-sharding-readwrite/interval-sharding-readwrite-paymentdb.sql
@@ -22,6 +22,9 @@ insert into `t_bill_0` (`id`, `order_id`, `lables`, `create_time`, `update_time`
 insert into `t_bill_1` (`id`, `order_id`, `lables`, `create_time`, `update_time`) values (10000009, 10000001, 'label-10000009 from r0z0mgr0db0.t1', '2020-03-05 10:00:00', '2020-03-05 10:00:00');
 insert into `t_bill_2` (`id`, `order_id`, `lables`, `create_time`, `update_time`) values (27000018, 10000002, 'label-27000018 from r0z0mgr0db0.t2', '2020-04-05 10:00:00', '2020-04-05 10:00:00');
 insert into `t_bill_3` (`id`, `order_id`, `lables`, `create_time`, `update_time`) values (37000019, 10000003, 'label-37000019 from r0z0mgr0db0.t3', '2021-09-05 10:00:00', '2021-09-05 10:00:00');
+-- ......
+-- ......
+insert into `t_bill_2192` (`id`, `order_id`, `lables`, `create_time`, `update_time`) values (37000019, 10000003, 'label-37000019 from r0z0mgr0db0.t2192', '2021-09-05 10:00:00', '2021-09-05 10:00:00');
 
 -- mgr0.db1
 drop schema if exists paymentdb_r0z0mgr0db1;
@@ -39,6 +42,9 @@ insert into `t_bill_0` (`id`, `order_id`, `lables`, `create_time`, `update_time`
 insert into `t_bill_1` (`id`, `order_id`, `lables`, `create_time`, `update_time`) values (10000029, 10000005, 'label-10000029 from r0z0mgr0db1.t1', '2021-02-05 10:00:00', '2021-02-05 10:00:00');
 insert into `t_bill_2` (`id`, `order_id`, `lables`, `create_time`, `update_time`) values (25000029, 10000006, 'label-25000029 from r0z0mgr0db1.t2', '2022-02-05 09:00:00', '2022-02-05 09:00:00');
 insert into `t_bill_3` (`id`, `order_id`, `lables`, `create_time`, `update_time`) values (35000029, 10000007, 'label-35000029 from r0z0mgr0db1.t3', '2022-02-05 10:00:00', '2022-02-05 10:00:00');
+-- ......
+-- ......
+insert into `t_bill_2192` (`id`, `order_id`, `lables`, `create_time`, `update_time`) values (37000019, 10000003, 'label-37000019 from r0z0mgr0db0.t2192', '2022-07-05 10:00:00', '2021-09-05 10:00:00');
 
 -- --------------------------------- Non Sharding single database --------------------------------------
 
